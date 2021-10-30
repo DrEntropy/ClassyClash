eafbcc2b50ca72e14e6c97eaa9e030a347718d18
Dr Entropy <rjljr2@gmail.com>
2021-10-30 09:01:09 -0700

    moved tick/run out of class decl

--- main.cpp
+++ main.cpp
@@ -26,57 +26,8 @@ class Character {
 
         // update knight 
 
-        void tick(float deltaTime) {
-
-            // update worldpos
-
-            Vector2 direction{};
-
-            if(IsKeyDown(KEY_A)) direction.x -= 1.0;
-            if(IsKeyDown(KEY_D)) direction.x += 1.0;
-            if(IsKeyDown(KEY_W)) direction.y -= 1.0;
-            if(IsKeyDown(KEY_S)) direction.y += 1.0;
-            
-            if(Vector2Length(direction)!=0.0)
-                {
-                    // set mapPos = mapPos - direction
-                    worldPos = Vector2Subtract(worldPos,
-                                Vector2Scale(Vector2Normalize(direction),speed));
-                                
-                    rightLeft = (direction.x < 0.f ? -1.f : 1.f);
-                    texture=run;
-                } else {
-                    texture=idle;
-                }
-
-            // update animation frame
-            runningTime += deltaTime;
-            if(runningTime>updateTime){
-                runningTime=0;
-                frame++;
-                if(frame >= maxFrames) frame =0;
-            }
-           
-
-        }
-
-        void draw(){
-                     // draw the character
-        
-                float knightWidth = (float)texture.width/6.0f;
-                Rectangle source { frame*knightWidth,0.0f, 
-                    rightLeft * knightWidth,
-                    (float)texture.height
-                    } ;
-
-                Rectangle dest{ screenPos.x, dest.y = screenPos.y,
-                    4.0f*knightWidth,
-                    4.0f*source.height
-                    };
-
-                DrawTexturePro(texture,source,dest,{0,0},0.0f,WHITE);
-
-        }
+        void tick(float deltaTime);
+        void draw();
 
 
     private:
@@ -101,7 +52,57 @@ class Character {
 
 };
 
+void Character::tick(float deltaTime) {
 
+    // update worldpos
+
+    Vector2 direction{};
+
+    if(IsKeyDown(KEY_A)) direction.x -= 1.0;
+    if(IsKeyDown(KEY_D)) direction.x += 1.0;
+    if(IsKeyDown(KEY_W)) direction.y -= 1.0;
+    if(IsKeyDown(KEY_S)) direction.y += 1.0;
+    
+    if(Vector2Length(direction)!=0.0)
+        {
+            // set mapPos = mapPos - direction
+            worldPos = Vector2Subtract(worldPos,
+                        Vector2Scale(Vector2Normalize(direction),speed));
+                        
+            rightLeft = (direction.x < 0.f ? -1.f : 1.f);
+            texture=run;
+        } else {
+            texture=idle;
+        }
+
+    // update animation frame
+    runningTime += deltaTime;
+    if(runningTime>updateTime){
+        runningTime=0;
+        frame++;
+        if(frame >= maxFrames) frame =0;
+    }
+    
+
+}
+
+void Character::draw(){
+                // draw the character
+
+        float knightWidth = (float)texture.width/6.0f;
+        Rectangle source { frame*knightWidth,0.0f, 
+            rightLeft * knightWidth,
+            (float)texture.height
+            } ;
+
+        Rectangle dest{ screenPos.x, dest.y = screenPos.y,
+            4.0f*knightWidth,
+            4.0f*source.height
+            };
+
+        DrawTexturePro(texture,source,dest,{0,0},0.0f,WHITE);
+
+}
 
 
 
